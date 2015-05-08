2402cb8cbfc44fb6b1b260433d22df424b22d02e
mjewkes <mjewkes@users.noreply.github.com>
2015-05-08 17:37:23 -0300

    Decode message

--- src/text_secure_v3/mod.rs
+++ src/text_secure_v3/mod.rs
@@ -1,6 +1,8 @@
 use ::axolotl;
 use ::axolotl::DH;
 use ::axolotl::DHKeyPair;
+use ::crypto_wrappers::aes_cbc;
+use ::crypto_wrappers::hmac;
 
 pub struct TextSecureV3;
 
@@ -41,13 +43,19 @@ pub struct Rootkey ([u8;32]);
 pub struct ChainKey ([u8;32]);
 
 #[derive(Clone)]
-pub struct MessageKey ([u8;32]);
+pub struct MessageKey{
+	cipher_key : [u8;32],
+	mac_key : [u8;32],
+	iv : [u8;16],
+}
 
 pub struct PlainText(Box<[u8]>);
 
-pub struct CipherText(Box<[u8]>);
-
-
+pub struct CipherTextAndVersion{
+	cipher_text : Box<[u8]>,
+	mac : [u8;8],
+	version : u8,
+}
 
 impl axolotl::Axolotl for TextSecureV3{
 	type IdentityKey = IdentityKey;
@@ -58,7 +66,7 @@ impl axolotl::Axolotl for TextSecureV3{
 	type MessageKey = MessageKey;
 
 	type PlainText = PlainText;
-	type CipherText = CipherText;
+	type CipherText = CipherTextAndVersion;
 
 
 	fn kdf_initial(ab0 : &<Self::IdentityKey as DH>::Shared, a0b : &<Self::IdentityKey as DH>::Shared, a0b0 : &<Self::IdentityKey as DH>::Shared) -> (Self::RootKey, Self::ChainKey){
@@ -76,8 +84,17 @@ impl axolotl::Axolotl for TextSecureV3{
 	fn encode_message(message_key : &Self::MessageKey, identity_key_local : &<Self::IdentityKey as DH>::Private, plaintext : &Self::PlainText) -> Self::CipherText{
 		unimplemented!();
 	}
-	fn decode_message(message_key : &Self::MessageKey, identity_key_remote : &<Self::IdentityKey as DH>::Public, cyphertext : &Self::CipherText) -> Option<Self::PlainText>{
-		unimplemented!();
+	fn decode_message(message_key : &Self::MessageKey, identity_key_remote : &<Self::IdentityKey as DH>::Public, ciphertext : &Self::CipherText) -> Option<Self::PlainText>{
+		if ciphertext.version != 3{
+			return None;
+		}
+		let result = aes_cbc::decrypt_aes256_cbc_mode(&ciphertext.cipher_text, message_key.cipher_key, message_key.iv);
+		match (result){
+			Ok (r) => {
+				return Some(PlainText(r.into_boxed_slice()))
+			},
+			Err (e) => None
+		}
 	}
 
 	fn ratchet_keys_are_equal(key0 : &<Self::RatchetKey as DH>::Public, key1 : &<Self::RatchetKey as DH>::Public) -> bool{
