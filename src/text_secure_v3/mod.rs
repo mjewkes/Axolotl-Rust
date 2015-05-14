f8b28156f0fc0524b4101243033c69e59fdb45a0
Jazz Alyxzander Turner-Baggs <jazz.alyxzander@gmail.com>
2015-05-14 17:30:20 -0300

    tabs to spaces

--- src/text_secure_v3/mod.rs
+++ src/text_secure_v3/mod.rs
@@ -81,101 +81,101 @@ pub struct CipherTextMacAndVersion{
 }
 
 impl axolotl::Axolotl for TextSecureV3{
-	type IdentityKey = IdentityKey;
-	type RatchetKey = RatchetKey;
+    type IdentityKey = IdentityKey;
+    type RatchetKey = RatchetKey;
 
-	type RootKey = Rootkey;
-	type ChainKey = ChainKey;
-	type MessageKey = MessageKey;
+    type RootKey = Rootkey;
+    type ChainKey = ChainKey;
+    type MessageKey = MessageKey;
 
-	type PlainText = PlainText;
-	type CipherText = CipherTextMacAndVersion;
+    type PlainText = PlainText;
+    type CipherText = CipherTextMacAndVersion;
 
     type Mac = ();
 
-	 fn derive_initial_root_key_and_chain_key(
+     fn derive_initial_root_key_and_chain_key(
         local_identity_remote_handshake_dh_secret : &DHShared<Self::IdentityKey>, 
         local_handshake_remote_identity_dh_secred : &DHShared<Self::IdentityKey>, 
         local_handshake_remote_handshake_dh_secret : &DHShared<Self::IdentityKey>) -> (Self::RootKey, Self::ChainKey){
-    	
+        
         unimplemented!();
-	}
+    }
 
-	fn derive_next_root_key_and_chain_key(root_key : Self::RootKey, ratchet : &<Self::RatchetKey as DH>::Shared) -> (Self::RootKey, Self::ChainKey){
-		unimplemented!();
-	}
+    fn derive_next_root_key_and_chain_key(root_key : Self::RootKey, ratchet : &<Self::RatchetKey as DH>::Shared) -> (Self::RootKey, Self::ChainKey){
+        unimplemented!();
+    }
 
-	fn derive_next_chain_and_message_key(chain_key : &Self::ChainKey) -> (Self::ChainKey, Self::MessageKey){
-		unimplemented!();
-	}
-	
-	fn encrypt_message(message_key : &Self::MessageKey, 
-		              plaintext : &Self::PlainText) 
-	                  -> Self::CipherText{
+    fn derive_next_chain_and_message_key(chain_key : &Self::ChainKey) -> (Self::ChainKey, Self::MessageKey){
+        unimplemented!();
+    }
+    
+    fn encrypt_message(message_key : &Self::MessageKey, 
+                      plaintext : &Self::PlainText) 
+                      -> Self::CipherText{
 
-		// let PlainText(ref text) = *plaintext;
-		// let cipher_data_result = aes_cbc::encrypt_aes256_cbc_mode(text,message_key.cipher_key, message_key.iv);
-		
-		//  //
-		// let mac = [0_u8;8];
-		// let cipher_text_out = CipherText{
-		// 	version=3,
-		// 	cipher_text=cipher_data_result.unwrap(),
+        // let PlainText(ref text) = *plaintext;
+        // let cipher_data_result = aes_cbc::encrypt_aes256_cbc_mode(text,message_key.cipher_key, message_key.iv);
+        
+        //  //
+        // let mac = [0_u8;8];
+        // let cipher_text_out = CipherText{
+        //  version=3,
+        //  cipher_text=cipher_data_result.unwrap(),
 
-		// }
-		// unimplemented!();
-		// //
-		
-		// let cipher_text_out = CipherTextMacAndVersion {
-		// 	version : 3,
-		// 	cipher_text : cipher_data_result.unwrap().into_boxed_slice(),
-		// 	mac : mac,
-		// };
+        // }
+        // unimplemented!();
+        // //
+        
+        // let cipher_text_out = CipherTextMacAndVersion {
+        //  version : 3,
+        //  cipher_text : cipher_data_result.unwrap().into_boxed_slice(),
+        //  mac : mac,
+        // };
         unimplemented!();
 
-	}
-	// fn mac_from_keys_and_bytes (cipher_text_bytes : &[u8],
-	//                             sender_public_key : &<Self::IdentityKey as DH>::Public,
-	//                             receiver_public_key : &<Self::IdentityKey as DH>::Public,
-	//                             )-> &[u8]{
+    }
+    // fn mac_from_keys_and_bytes (cipher_text_bytes : &[u8],
+    //                             sender_public_key : &<Self::IdentityKey as DH>::Public,
+    //                             receiver_public_key : &<Self::IdentityKey as DH>::Public,
+    //                             )-> &[u8]{
 
-	// }
-	fn decrypt_message(message_key : &Self::MessageKey, 
-		              ciphertext : &Self::CipherText) 
-	                  -> Option<Self::PlainText>{
+    // }
+    fn decrypt_message(message_key : &Self::MessageKey, 
+                      ciphertext : &Self::CipherText) 
+                      -> Option<Self::PlainText>{
 
-  //     	{
-		// 	if ciphertext.version != 3{
-		// 		return None;
-		// 	}
-		// }
-		// {
-		// 	//HMAC - this is not correct.
-		// 	unimplemented!();
-		// 	let ref data = ciphertext.cipher_text;
-		// 	let their_mac = ciphertext.mac;
-		// 	let mut mac_context=
-		// 		hmac::HmacSha256::new(&message_key.mac_key);
-		// 	mac_context.input(identity_key_remote);
-		// 	mac_context.input(data);
+  //        {
+        //  if ciphertext.version != 3{
+        //      return None;
+        //  }
+        // }
+        // {
+        //  //HMAC - this is not correct.
+        //  unimplemented!();
+        //  let ref data = ciphertext.cipher_text;
+        //  let their_mac = ciphertext.mac;
+        //  let mut mac_context=
+        //      hmac::HmacSha256::new(&message_key.mac_key);
+        //  mac_context.input(identity_key_remote);
+        //  mac_context.input(data);
 
-		// 	let mac_result = mac_context.result();
-		// 	let mac_data = mac_result.code();
-		// 	let first_8_bytes = &mac_data[0..8];
-		// 	if first_8_bytes != ciphertext.mac {
-		// 		return None;
-		// 	}
-		// }
+        //  let mac_result = mac_context.result();
+        //  let mac_data = mac_result.code();
+        //  let first_8_bytes = &mac_data[0..8];
+        //  if first_8_bytes != ciphertext.mac {
+        //      return None;
+        //  }
+        // }
 
-		// let result = aes_cbc::decrypt_aes256_cbc_mode(&ciphertext.cipher_text, message_key.cipher_key, message_key.iv);
-		// match (result){
-		// 	Ok (r) => {
-		// 		Some(PlainText(r.into_boxed_slice()))
-		// 	},
-		// 	Err (e) => None
-		// }
+        // let result = aes_cbc::decrypt_aes256_cbc_mode(&ciphertext.cipher_text, message_key.cipher_key, message_key.iv);
+        // match (result){
+        //  Ok (r) => {
+        //      Some(PlainText(r.into_boxed_slice()))
+        //  },
+        //  Err (e) => None
+        // }
         unimplemented!();
-	}
+    }
 
     fn authenticate_message(
         message : &AxolotlMessage<Self>, 
@@ -186,22 +186,22 @@ impl axolotl::Axolotl for TextSecureV3{
         unimplemented!();
     }
 
-	fn ratchet_keys_are_equal(key0 : &<Self::RatchetKey as DH>::Public, key1 : &<Self::RatchetKey as DH>::Public) -> bool{
-		unimplemented!();
-	}
-	fn generate_ratchet_key_pair() -> DHKeyPair<Self::RatchetKey>{
-		unimplemented!();
-	}
+    fn ratchet_keys_are_equal(key0 : &<Self::RatchetKey as DH>::Public, key1 : &<Self::RatchetKey as DH>::Public) -> bool{
+        unimplemented!();
+    }
+    fn generate_ratchet_key_pair() -> DHKeyPair<Self::RatchetKey>{
+        unimplemented!();
+    }
 
-	fn future_message_limit() -> u32{
-		unimplemented!();
-	}
-	fn chain_message_limit() -> u32
-	{
-		2000
-	}
+    fn future_message_limit() -> u32{
+        unimplemented!();
+    }
+    fn chain_message_limit() -> u32
+    {
+        2000
+    }
 
-	fn skipped_chain_limit() -> usize{
-		unimplemented!();
-	}
+    fn skipped_chain_limit() -> usize{
+        unimplemented!();
+    }
 }
